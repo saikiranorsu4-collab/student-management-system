c65212ee0d71db496f3956dab64876e44d2e9734
Saikiran <saikiranorsu4@example.com>
2026-05-18 09:50:36 +0530

    Improved ProtectedRoute

--- student-frontend/src/components/ProtectedRoute.js
+++ student-frontend/src/components/ProtectedRoute.js
@@ -3,12 +3,15 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
 
+  // CHECK TOKEN
   const token = localStorage.getItem("token");
 
+  // IF NO TOKEN → REDIRECT TO LOGIN
   if (!token) {
-    return <Navigate to="/" />;
+    return <Navigate to="/" replace />;
   }
 
+  // IF TOKEN EXISTS → SHOW PAGE
   return children;
 };
 
